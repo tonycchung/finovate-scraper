8b8fafe67f79c390f0e942c4bea76bb9a9ce8910
Tony Chung <anthonyuchung@gmail.com>
2014-12-08 03:04:59 -0800

    Complete asia 2012

--- test.rb
+++ test.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://www.finovate.com/asia13vid/appannie.html"))
+doc = Nokogiri::HTML(open("http://www.finovate.com/asia12vid/ubank.html"))
 company_details = doc.css('#contentwrapper > table > tr > td > table:nth-child(3) > tr > td > table > tr > td:nth-child(2)').inner_html
 
 # Save entire company profile from td, add it line by line to company_profile until we've captured only necessary profile data
 # (nothing from and after "Product distribution strategy:")
 company_profile = ""
-p doc.css('#contentwrapper > table > tr > td > table:nth-child(3) > tr > td > div > div > table > tr > td.cellpadding-left')
+p doc.css('#contentwrapper > table > tr > td > table:nth-child(3) > tr > td > div > table > tr > td.cellpadding-left').inner_html
