48c5de633e3f0087aef464fa81e1c3b044415327
Tony Chung <anthonyuchung@gmail.com>
2014-12-08 18:01:25 -0800

    Redo fall2k14

--- fall2k14_done_novids/adder.rb
+++ fall2k14_done_novids/adder.rb
@@ -3,17 +3,14 @@ require_relative '../add_vid_thumb_tax.rb'
 csv = 'fall2k14'
 adder = Adder.new(csv)
 
-# p 'adding video'
-# adder.add_video
+p 'adding video'
+adder.add_video
 
-# p 'adding thumbs'
-# adder.add_thumb
+p 'adding thumbs'
+adder.add_thumb
 
-# p 'adding tax'
-# adder.add_tax
-
-# p 'checking missing'
-# adder.check_missing(csv + "_plus_video_thumb_tax.csv")
+p 'adding tax'
+adder.add_tax
 
 p 'checking missing'
 adder.check_missing('fall2k14_master.csv')
