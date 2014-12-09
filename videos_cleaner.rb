9b063cbaa86a3f089c95b7a9dcbcab0c005fe255
Tony Chung <anthonyuchung@gmail.com>
2014-12-09 15:44:44 -0800

    Complete finovate2k7

--- videos_cleaner.rb
+++ videos_cleaner.rb
@@ -1,5 +1,14 @@
 require 'csv'
 
-CSV.foreach('videos.csv') do |row|
-  row[1] = row[1].match /^.*[^\.mov]/
+arry = CSV.read('videos.csv')
+new_arry = []
+
+arry.each do |row|
+  new_arry << row if row.match /best/i
+end
+
+CSV.open('best_of_videos.csv', 'w').each do |csv|
+  new_arry.each do |row|
+    csv << row
+  end
 end
