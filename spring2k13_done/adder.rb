a964ae90407f9330cc622681ceb37c397800280f
Tony Chung <anthonyuchung@gmail.com>
2014-12-08 17:41:48 -0800

    Redo spring2k13

--- spring2k13_done/adder.rb
+++ spring2k13_done/adder.rb
@@ -1,18 +1,16 @@
 require_relative '../add_vid_thumb_tax.rb'
 
-adder = Adder.new('spring2k13')
+csv = 'spring2k13'
+adder = Adder.new(csv)
 
-# p 'adding video'
-# adder.add_video
+p 'adding video'
+adder.add_video
 
-# p 'adding thumbs'
-# adder.add_thumb
+p 'adding thumbs'
+adder.add_thumb
 
-# p 'adding tax'
-# adder.add_tax
-
-# p 'checking missing'
-# adder.check_missing('spring2k13_plus_video_thumb_tax.csv')
+p 'adding tax'
+adder.add_tax
 
 p 'checking missing'
-adder.check_missing('spring2k13_master.csv')
+adder.check_missing("#{csv}_master.csv")
